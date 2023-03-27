a98d3fc18dc632db8f53ebbe1f5731db4c407ad0
alexpineda99 <alexandropinedam1310@gmail.com>
2023-03-27 09:02:20 -0400

    Profile added

--- src/Components/userComponents/Profile.jsx
+++ src/Components/userComponents/Profile.jsx
@@ -1,4 +1,23 @@
+import React from "react";
+import axios from "axios";
+import { useSelector } from "react-redux";
+
 function Profile () {
+    
+    const user = useSelector(state=> state.userState.user)
+
+    console.log(user)
+
+    axios.get("http://localhost:3001/profile", {
+        headers: {"auth": user},
+    })
+    .then(res => {
+        console.log(res)
+    })
+    .catch(err=> {
+        console.log(err.response.data)
+    })
+
 
     return (
 
